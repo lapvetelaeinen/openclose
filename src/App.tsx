dc2492ba81d360f63cb5f22f7c2516400cd3dfed
Filip <filip@norddev.se>
2021-11-30 11:02:16 +0100

    adding build files

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,102 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import React, { useState } from "react";
+import {
+  IonApp,
+  IonHeader,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonSelect,
+  IonSelectOption,
+  IonTextarea,
+  IonButton,
+} from "@ionic/react";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
+import TaskCard from "./components/TaskCard";
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
+const App: React.FC = () => {
+  const [text, setText] = useState<string>();
+
+  function AddCard(){
+return(
+<h2>Hello!!!</h2>
 );
+  };
+
+  return (
+    <IonApp>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Lägg till uppgift</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Ansvarsområde</IonLabel>
+                <IonSelect value="bar" okText="Ok" cancelText="Avbryt">
+                  <IonSelectOption value="bar">Bar</IonSelectOption>
+                  <IonSelectOption value="golv">Golv</IonSelectOption>
+                  <IonSelectOption value="gardeob">Garderob</IonSelectOption>
+                  <IonSelectOption value="kök">Kök</IonSelectOption>
+                  <IonSelectOption value="disk">Disk</IonSelectOption>
+                </IonSelect>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Uppgiftens namn</IonLabel>
+                <IonInput></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Beskrivning</IonLabel>
+                <IonTextarea
+                  value={text}
+                  onIonChange={(e) => setText(e.detail.value!)}
+                ></IonTextarea>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonButton onClick={AddCard}>Click me!</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonApp>
+  );
+};
 
 export default App;
---
+++ src/components/TaskCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TaskCard() {
+    return (
+        <div>
+            <h2>Task Card!</h2>
+        </div>
+    )
+}
